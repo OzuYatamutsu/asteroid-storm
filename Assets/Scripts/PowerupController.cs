526e43c9c86f30dbf1c40bca1ad62737624e3f4d
Sean Collins <seanc@seancotech.com>
2020-05-01 19:06:37 -0400

    Add Powerup scripts

---
+++ Assets/Scripts/PowerupController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls movement and handling of Powerup objects created
+/// in the game field.
+/// </summary>
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Rigidbody))]
+public class PowerupController : MonoBehaviour
+{
+    /// <summary>
+    /// Powerups should not move if game is paused.
+    /// </summary>
+    private bool ShouldMove;
+
+    private void OnEnable()
+    {
+        EventManager.onGameStart += OnGameResumeOrStarted;
+        EventManager.onGamePaused += OnGamePaused;
+        EventManager.onGameResume += OnGameResumeOrStarted;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.onGameStart -= OnGameResumeOrStarted;
+        EventManager.onGamePaused -= OnGamePaused;
+        EventManager.onGameResume -= OnGameResumeOrStarted;
+    }
+
+    /// <summary>
+    /// Which direction should this Powerup move?
+    /// </summary>
+    public Vector3 MovementVector {get; private set;}
+    private Vector3 RotationVector;
+
+    /// <summary>
+    /// How fast is the Powerup moving along its
+    /// movement vector?
+    /// </summary>
+    public float Speed {get; private set;}
+
+    private Renderer IsVisibleRenderer;
+    private Rigidbody ChildRigidBody;
+
+    /// <summary>
+    /// Called when the Powerup is first created.
+    /// </summary>
+    void Start()
+    {
+        // Set renderer component
+        IsVisibleRenderer = gameObject.GetComponent<Renderer>();
+
+        // Set movement vector of the Powerup
+        MovementVector = Quaternion.Euler(
+            Random.Range(-45,45),  // x
+            Random.Range(-45,45),  // y
+            Random.Range(-45,45)  // z
+        ) * new Vector3(
+            transform.forward.x * Random.Range(-10.0f, 10.0f),
+            transform.forward.y * Random.Range(-10.0f, 10.0f),
+            transform.forward.z * Random.Range(-10.0f, 10.0f)
+        );
+
+        // Set rotation vector of the Powerup
+        RotationVector = Random.insideUnitSphere;
+
+        // Set rigidbody
+        ChildRigidBody = gameObject.GetComponent<Rigidbody>();
+
+        // How will the powerup rotate?
+        ChildRigidBody.angularVelocity = RotationVector;
+
+        // How will the powerup move?
+        ChildRigidBody.velocity = MovementVector;
+    }
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    void Update() {}
+
+    void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnGamePaused()
+    {
+        // Stop powerup from moving
+        ChildRigidBody.velocity = Vector3.zero;
+    }
+
+    private void OnGameResumeOrStarted()
+    {
+        // Start powerup moving again
+        ChildRigidBody.velocity = MovementVector;
+    }
+}
---
+++ Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Manages how Powerup objects are spawned in.
+/// </summary>
+public class PowerupManager : MonoBehaviour
+{
+    /// <summary>
+    /// What should be spawned in?
+    /// </summary>
+    [SerializeField] public PowerupController SpawnTarget;
+
+    /// <summary>
+    /// Holds references to the Powerups currently on the game field.
+    /// </summary>
+    private List<PowerupController> ActivePowerups;
+
+    private static System.Random _rand = new System.Random();
+
+    // The following are parameters that are used to determine
+    // where the Powerup objects should be spawned in.
+    private const float SPAWN_DISTANCE_MAX = 750f;
+    private const float DESPAWN_DISTANCE_MAX = 750f;  // (Behind the player.)
+
+    /// <summary>
+    /// Are we allowed to change the state of Powerups in the scene?
+    /// </summary>
+    private bool SpawningEnabled;
+
+    private void OnEnable()
+    {
+        EventManager.onGameStart += OnGameStart;
+        EventManager.onGamePaused += OnGamePausedOrEnded;
+        EventManager.onGameResume += OnGameResume;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.onGameStart -= OnGameStart;
+        EventManager.onGamePaused -= OnGamePausedOrEnded;
+        EventManager.onGameResume -= OnGameResume;
+    }
+
+    /// <summary>
+    /// Called when the PowerupManager is first created.
+    /// </summary>
+    private void Start()
+    {
+        ActivePowerups = new List<PowerupController>();
+    }
+
+    private void LateUpdate()
+    {
+        if (SpawningEnabled)
+        {
+            // Despawn powerups if necessary
+            foreach (PowerupController powerup in GetPowerupsToDespawn())
+                DespawnPowerup(powerup);
+
+            // Spawn powerups if necessary
+            SpawnPowerups();
+        }
+    }
+
+    /// <summary>
+    /// Spawns in a distance around the player.
+    /// </summary>
+    private void SpawnPowerups()
+    {
+        // 1% chance to spawn powerup
+        if (_rand.Next(1, 100) < 99)
+            return;
+
+        Debug.Log("Spawning powerup!");
+        SpawnPowerup();
+    }
+
+    /// <summary>
+    /// Spawns a new Powerup somewhere on the game field.
+    /// </summary>
+    private void SpawnPowerup()
+    {
+        // Where should the Powerup be spawned relative to the player?
+        var playerTransform = GameManager.Instance.PlayerShip.transform;
+        var spawnLocation = new Vector3();
+
+        while (!GameManager.Instance.FollowCam.IsVisibleFromFollowCam(spawnLocation)) {
+            // Only spawn at max distance in front of player.
+            spawnLocation = playerTransform.position + (playerTransform.forward * SPAWN_DISTANCE_MAX);
+
+            // Add a random angle to spawn location so we don't spawn directly in front of player
+            spawnLocation = Quaternion.Euler(
+                Random.Range(-45,45),  // x
+                Random.Range(-45,45),  // y
+                Random.Range(-45,45)  // z
+            ) * spawnLocation;
+        }
+        
+        ActivePowerups.Add(
+            Instantiate(SpawnTarget, spawnLocation, Quaternion.identity, transform)
+        );
+    }
+
+    /// <summary>
+    /// Returns an array of Powerup objects that are outside of maximum distance
+    /// to the player, and are candidates for despawn.
+    /// </summary>
+    private PowerupController[] GetPowerupsToDespawn()
+    {
+        return ActivePowerups.Where(powerup => PowerupIsTooFar(powerup))
+                             .Where(powerup => !GameManager.Instance.FollowCam.IsVisibleFromFollowCam(
+                                  powerup.gameObject.transform.position
+                              ))
+                             .ToArray();
+    }
+
+    /// <summary>
+    /// Returns true if the powerup is beyond DESPAWN_DISTANCE_MAX in any direction.
+    /// </summary>
+    private bool PowerupIsTooFar(PowerupController powerup)
+    {
+        var relativeDistanceToPlayer = GameManager.Instance.PlayerShip.GetRelativeDistance(
+            powerup.transform.position, true
+        );
+
+        return (
+            relativeDistanceToPlayer.x >= DESPAWN_DISTANCE_MAX
+            || relativeDistanceToPlayer.y >= DESPAWN_DISTANCE_MAX
+            || relativeDistanceToPlayer.z >= DESPAWN_DISTANCE_MAX
+        );
+    }
+
+    /// <summary>
+    /// Despawns a specific Powerup somewhere on the game field.
+    /// </summary>
+    private void DespawnPowerup(PowerupController powerup)
+    {
+        Destroy(powerup);
+        ActivePowerups.Remove(powerup);
+    }
+
+    private void OnGameStart()
+    {
+        SpawningEnabled = true;
+    }
+
+    private void OnGamePausedOrEnded()
+    {
+        SpawningEnabled = false;
+    }
+
+    private void OnGameResume()
+    {
+        SpawningEnabled = true;
+    }
+}
